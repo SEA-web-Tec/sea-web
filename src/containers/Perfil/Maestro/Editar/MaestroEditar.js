889f25b4d3aa4dab8ea615a04f0b09993746febf
NightlyFlux <nightlyflux@gmail.com>
2020-12-01 19:37:50 -0800

    Agregada la funcionalidad con la API

--- src/containers/Perfil/Maestro/Editar/MaestroEditar.js
+++ src/containers/Perfil/Maestro/Editar/MaestroEditar.js
@@ -1,26 +1,39 @@
-import React, { Component } from "react";
-import { updateObject } from "shared/utility";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import * as actions from "store/actions/index";
+import { getBase64, updateObject } from "shared/utility";
 import { withStyles } from "@material-ui/core/styles";
 import { useStyles } from "./Styles";
-import { Grid, TextField, InputAdornment, IconButton, Badge, Avatar, Typography, Button, Box } from "@material-ui/core";
 import {
-  Add as AddIcon,
+  Grid,
+  TextField,
+  InputAdornment,
+  IconButton,
+  Badge,
+  Avatar,
+  Typography,
+  Button,
+  Box,
+  CircularProgress,
+  Snackbar
+} from "@material-ui/core";
+import {
   AddCircleTwoTone as AddCircleTwoToneIcon,
   Visibility as VisibilityIcon,
   VisibilityOff as VisibilityOffIcon
 } from "@material-ui/icons";
 import PerfilPortada from "components/Perfil/PerfilPortada/PerfilPortada";
+import { Alert } from "@material-ui/lab";
+import { http } from "shared/http";
 
 class MaestroEditar extends Component {
   state = {
-    fotoPerfil: "https://source.unsplash.com/random",
-    fotoPortada: "https://source.unsplash.com/random",
-    nombre: "José Tadeo",
-    apellidoPaterno: "Rodriguez",
-    apellidoMaterno: "Solano",
-    departamentoAcademico: "ISC",
-    correo: "tadeo@lapaz.tecnm.mx",
-    contrasenia: "123123",
+    fotoPerfil: "",
+    fotoPortada: "",
+    correo: "",
+    contrasenia: "",
+    repetirContrasenia: "",
+    error: false,
     showPassword: false
   };
 
@@ -33,7 +46,23 @@ class MaestroEditar extends Component {
 
   submitHandler = (event) => {
     event.preventDefault();
-    this.props.onAuth(this.state.correo, this.state.contrasenia);
+    if (this.state.contrasenia === this.state.repetirContrasenia) {
+      // TODO: Hacer esta wea en redux
+      // this.props.onAuth(this.state.correo, this.state.contrasenia);
+
+      http
+        .patch(`/usuarios/${this.props.user.id}/editar`, {
+          fotoPerfil: this.state.fotoPerfil,
+          fotoPortada: this.state.fotoPortada,
+          correo: this.state.correo,
+          contrasenia: this.state.contrasenia
+        })
+        .then((response) => {
+          this.props.history.push(`/usuario/${this.props.user.id}`);
+        });
+    } else {
+      this.setState({ error: true });
+    }
   };
 
   showPasswordHandler = (event) => {
@@ -43,124 +72,207 @@ class MaestroEditar extends Component {
     });
   };
 
+  handleFileSelected = (event, prop) => {
+    if (event.target.files.length > 0) {
+      getBase64(event.target.files[0]).then((data) => {
+        this.setState({ [prop]: data.slice(23).toString() });
+      });
+    }
+  };
+
   render(props) {
     const { classes } = this.props;
 
-    return (
-      <PerfilPortada
-        fotoPerfil={this.state.fotoPerfil}
-        fotoPortada={this.state.fotoPortada}
-        departamentoAcademico={this.state.departamentoAcademico}
-        nombre={this.state.nombre}
-        apellidoPaterno={this.state.apellidoPaterno}
-        apellidoMaterno={this.state.apellidoMaterno}
+    let error = (
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        open={this.state.error}
+        onClose={() => {
+          this.setState({ error: false });
+        }}
+        autoHideDuration={4000}
       >
-        <form>
-          <Grid container spacing={2}>
-            <Grid item xd={12} sm={12} md={6} style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-              <Typography component="h6" variant="h6" align="center" gutterBottom>
-                Foto de perfil
-              </Typography>
-              <Badge
-                overlap="circle"
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "right"
-                }}
-                badgeContent={
-                  <IconButton color="secondary" size="small" className={classes.badgeIcon}>
-                    <AddCircleTwoToneIcon fontSize="large" />
-                  </IconButton>
-                }
-                className={classes.center}
-              >
-                <Avatar src={this.state.fotoPerfil} alt="Foto de perfil" style={{ width: "200px", height: "200px" }} />
-              </Badge>
+        <Alert variant="filled" severity="error">
+          Favor de llenar los campos de manera correcta
+        </Alert>
+      </Snackbar>
+    );
+
+    let perfil = <CircularProgress className={classes.spinner} />;
+
+    if (!this.props.loading) {
+      perfil = (
+        <PerfilPortada
+          fotoPerfil={this.props.user.fotoPerfil}
+          fotoPortada={this.props.user.fotoPortada}
+          departamentoAcademico={this.props.user.departamentoAcademico}
+          nombres={this.props.user.nombres}
+          apellidoPaterno={this.props.user.apellidoPaterno}
+          apellidoMaterno={this.props.user.apellidoMaterno}
+        >
+          <form>
+            <Grid container spacing={2}>
+              <Grid item xd={12} sm={12} md={6} style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+                <Typography component="h6" variant="h6" align="center" gutterBottom>
+                  Foto de perfil
+                </Typography>
+                <Badge
+                  overlap="circle"
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right"
+                  }}
+                  badgeContent={
+                    <Fragment>
+                      <input
+                        accept="image/*"
+                        style={{ display: "none" }}
+                        id="fotoPerfil"
+                        type="file"
+                        onChange={(e) => this.handleFileSelected(e, "fotoPerfil")}
+                      />
+                      <label htmlFor="fotoPerfil">
+                        <IconButton color="secondary" size="small" className={classes.badgeIcon} component="span">
+                          <AddCircleTwoToneIcon fontSize="large" />
+                        </IconButton>
+                      </label>
+                    </Fragment>
+                  }
+                  className={classes.center}
+                >
+                  <Avatar
+                    src={`data:image/jpeg;base64,${this.state.fotoPerfil ? this.state.fotoPerfil : this.props.user.fotoPerfil}`}
+                    alt="Foto de perfil"
+                    style={{ width: "200px", height: "200px" }}
+                  />
+                </Badge>
+              </Grid>
+              <Grid item xd={12} sm={12} md={6} style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+                <Typography component="h6" variant="h6" align="center" gutterBottom>
+                  Foto de portada
+                </Typography>
+                <Badge
+                  overlap="rectangle"
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right"
+                  }}
+                  badgeContent={
+                    <Fragment>
+                      <input
+                        accept="image/*"
+                        style={{ display: "none" }}
+                        id="fotoPortada"
+                        type="file"
+                        onChange={(e) => this.handleFileSelected(e, "fotoPortada")}
+                      />
+                      <label htmlFor="fotoPortada">
+                        <IconButton color="secondary" size="small" className={classes.badgeIcon} component="span">
+                          <AddCircleTwoToneIcon fontSize="large" />
+                        </IconButton>
+                      </label>
+                    </Fragment>
+                  }
+                  className={classes.center}
+                >
+                  <img
+                    src={`data:image/jpeg;base64,${
+                      this.state.fotoPortada ? this.state.fotoPortada : this.props.user.fotoPortada
+                    }`}
+                    alt="Foto de portada"
+                    width="300px"
+                    height="100px"
+                    style={{ objectFit: "cover" }}
+                  />
+                </Badge>
+              </Grid>
+              <Grid item xd={12} sm={12} md={12}>
+                <Typography component="h6" variant="h6" align="center">
+                  Datos generales
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  id="correo"
+                  name="correo"
+                  label="Correo Electrónico"
+                  value={this.state.correo}
+                  onChange={this.inputChangedHandler("correo")}
+                  type="email"
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  id="contrasenia"
+                  name="contrasenia"
+                  label="Contraseña"
+                  value={this.state.contrasenia}
+                  onChange={this.inputChangedHandler("contrasenia")}
+                  type={this.state.showPassword ? "text" : "password"}
+                  error={this.state.contrasenia !== this.state.repetirContrasenia}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={this.showPasswordHandler}>
+                          {this.state.showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  id="repetirContrasenia"
+                  name="repetirContrasenia"
+                  label="Repetir Contraseña"
+                  value={this.state.repetirContrasenia}
+                  onChange={this.inputChangedHandler("repetirContrasenia")}
+                  type={this.state.showPassword ? "text" : "password"}
+                  error={this.state.contrasenia !== this.state.repetirContrasenia}
+                  helperText={this.state.contrasenia !== this.state.repetirContrasenia && "Las contraseña no coinciden"}
+                />
+              </Grid>
             </Grid>
-            <Grid item xd={12} sm={12} md={6} style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-              <Typography component="h6" variant="h6" align="center" gutterBottom>
-                Foto de portada
-              </Typography>
-              <Badge
-                overlap="rectangle"
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "right"
-                }}
-                badgeContent={
-                  <IconButton color="secondary" size="small" className={classes.badgeIcon}>
-                    <AddCircleTwoToneIcon fontSize="large" />
-                  </IconButton>
-                }
-                className={classes.center}
-              >
-                <img src={this.state.fotoPortada} width="300px" height="100px" style={{ objectFit: "cover" }} />
-              </Badge>
-            </Grid>
-            <Grid item xd={12} sm={12} md={12}>
-              <Typography component="h6" variant="h6" align="center">
-                Datos generales
-              </Typography>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="correo"
-                name="correo"
-                label="Correo Electrónico"
-                value={this.state.correo}
-                onChange={this.inputChangedHandler("correo")}
-                type="email"
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="contrasenia"
-                name="contrasenia"
-                label="Contraseña"
-                value={this.state.contrasenia}
-                onChange={this.inputChangedHandler("contrasenia")}
-                type={this.state.showPassword ? "text" : "password"}
-                // error={this.props.error !== null}
-                // helperText={this.props.error !== null && this.props.error.code === 400 ? "Contraseña Inválida" : ""}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={this.showPasswordHandler}>
-                        {this.state.showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                      </IconButton>
-                    </InputAdornment>
-                  )
-                }}
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="repetirContrasenia"
-                name="repetirContrasenia"
-                label="Repetir Contraseña"
-                value={this.state.repetirContrasenia}
-                onChange={this.inputChangedHandler("repetirContrasenia")}
-                type={this.state.showPassword ? "text" : "password"}
-                // error={this.props.error !== null}
-                // helperText={this.props.error !== null && this.props.error.code === 400 ? "Contraseña Inválida" : ""}
-              />
-            </Grid>
-          </Grid>
-          <Box display="flex" justifyContent="center" padding={2}>
-            <Button variant="contained" color="secondary" size="large">
-              Guardar Cambios
-            </Button>
-          </Box>
-        </form>
-      </PerfilPortada>
+            <Box display="flex" justifyContent="center" padding={2}>
+              <Button variant="contained" color="secondary" size="large" type="submit" onClick={this.submitHandler}>
+                Guardar Cambios
+              </Button>
+            </Box>
+          </form>
+        </PerfilPortada>
+      );
+    }
+    return (
+      <Fragment>
+        {error}
+        {perfil}
+      </Fragment>
     );
   }
 }
 
-export default withStyles(useStyles)(MaestroEditar);
+const mapStateToProps = (state) => {
+  return {
+    token: state.auth.token,
+    user: state.auth.user,
+    error: state.auth.error,
+    hasError: state.auth.hasError,
+    loading: state.auth.loading
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchPerfil: (token, userId) => dispatch(actions.fetchPerfil(token, userId)),
+    onPerfilDismissError: () => dispatch(actions.perfilDismissError())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(MaestroEditar));
