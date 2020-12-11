25a7c4e98c4ff16b1091f0ee15f6c358f6852095
Andrea Vargas Castro <16310731@itlp.edu.mx>
2020-12-10 19:30:33 -0600

    Limpié el código de OpcionRespuesta - Examenes

--- src/containers/Examen/CrearReactivo/OpcionRespuesta.js
+++ src/containers/Examen/CrearReactivo/OpcionRespuesta.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { useStyles } from "./Styles";
-import { Grid, FormControl, TextField, IconButton, OutlinedInput, InputLabel, InputAdornment } from "@material-ui/core";
+import { Grid, FormControl, TextField } from "@material-ui/core";
 
-const OpcionRespuesta = ({ props, /*array,*/ option , num }) => {
+const OpcionRespuesta = ({ props, option , num }) => {
     const classes = props;
         return (
             <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                 <FormControl required variant="outlined" className={classes.formControl}>
-                    <TextField id="outlined-basic" label={"Opción de respuesta "+option+')'} variant="outlined"
-                    required={num<4}/>
+                    <TextField id="outlined-basic" label={"Opción de respuesta "+ option +')'} variant="outlined"
+                    required={ num < 4 }/>
                 </FormControl>
             </Grid>
         );
