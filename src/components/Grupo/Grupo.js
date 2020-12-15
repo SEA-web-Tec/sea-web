882c8fb55b1d386b96ba52ac4e72fbf5d97471de
NightlyFlux <nightlyflux@gmail.com>
2020-12-14 23:27:30 -0800

    Ajuste en iconos de cada grupo

--- src/components/Grupo/Grupo.js
+++ src/components/Grupo/Grupo.js
@@ -9,11 +9,12 @@ import {
   InstrumentacionOutlined as InstrumentacionIcon,
   TemarioOutlined as TemarioIcon
 } from "../../assets/icons/Index";
-import { blue, red, green, amber } from "@material-ui/core/colors";
+import { blue, amber } from "@material-ui/core/colors";
 import BigTooltip from "../UI/BigTooltip/BigTooltip";
 
 const Grupo = (props) => {
   const classes = useStyles();
+
   return (
     <Grid item xs={12} sm={6} md={6} lg={4}>
       <Card className={classes.card}>
@@ -40,18 +41,18 @@ const Grupo = (props) => {
         </CardContent>
         <Divider />
         <CardActions className={classes.cardActions} disableSpacing>
-          <BigTooltip title="Calificaciones">
-            <IconButton style={{ color: blue[600] }}>
+          <BigTooltip title="Exámenes">
+            <IconButton style={{ color: blue[600] }} component={RouterLink} to={`/examen/${props.id}/crear`}>
               <CalificacionesIcon />
             </IconButton>
           </BigTooltip>
           <BigTooltip title="Lista">
-            <IconButton style={{ color: red[600] }}>
+            <IconButton disabled>
               <FormatListBulleted />
             </IconButton>
           </BigTooltip>
           <BigTooltip title="Temario">
-            <IconButton style={{ color: green[600] }}>
+            <IconButton disabled>
               <TemarioIcon />
             </IconButton>
           </BigTooltip>
