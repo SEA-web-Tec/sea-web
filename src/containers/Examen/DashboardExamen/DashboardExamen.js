9fdf73b09dc97b8ea8f56bf062588af75dc5c5a0
NightlyFlux <nightlyflux@gmail.com>
2020-12-14 23:00:48 -0800

    Creo que puro formato jiji

--- src/containers/Examen/DashboardExamen/DashboardExamen.js
+++ src/containers/Examen/DashboardExamen/DashboardExamen.js
@@ -5,8 +5,9 @@ import {
   ExpandMore as ExpandMoreIcon,
   Create as CreateIcon,
   GroupAdd as GroupAddIcon,
-  Delete as DeleteIcon } from "@material-ui/icons";
-import { Typography, Accordion, AccordionDetails, AccordionSummary, Box, IconButton, Divider} from "@material-ui/core";
+  Delete as DeleteIcon
+} from "@material-ui/icons";
+import { Typography, Accordion, AccordionDetails, AccordionSummary, Box, IconButton, Divider } from "@material-ui/core";
 import Portada from "../../../components/Portada/Portada";
 import ExamenesTable from "../../../components/Examenes/ExamenesTable/Table";
 import FloatingButton from "../../../components/Examenes/ExamFloatingButton/FloatingButton";
@@ -14,80 +15,83 @@ import FloatingButton from "../../../components/Examenes/ExamFloatingButton/Floa
 class DashboardExamen extends Component {
   render(props) {
     const { classes } = this.props;
-    const encabezados = ["Grupo","Fecha","Estado","Calificaciones"];
-    const examenes = [{
-      key:1,
-      grupo: "F", // no se almacena D:
-      fin: "10/04/2021",
-      estado: "Abierto"
-    }, {
-      key:2,
-      grupo: "G", 
-      fin: "12/04/2021",
-      estado: "Abierto"
-    }, {
-      key:3,
-      grupo: "H", 
-      fin: "12/12/2020",
-      estado: "Cerrado"
-    }, {
-      key:4,
-      grupo: "G", 
-      fin: "03/10/2020",
-      estado: "Cerrado"
-    }, {
-      key:5,
-      grupo: "E", 
-      fin: "09/07/2020",
-      estado: "Cerrado"
-    }, {
-      key:6,
-      grupo: "E", 
-      fin: "22/07/2019",
-      estado: "Cerrado"
-    }];
+    const encabezados = ["Grupo", "Fecha", "Estado", "Calificaciones"];
+    const examenes = [
+      {
+        key: 1,
+        grupo: "F", // no se almacena D:
+        fin: "10/04/2021",
+        estado: "Abierto"
+      },
+      {
+        key: 2,
+        grupo: "G",
+        fin: "12/04/2021",
+        estado: "Abierto"
+      },
+      {
+        key: 3,
+        grupo: "H",
+        fin: "12/12/2020",
+        estado: "Cerrado"
+      },
+      {
+        key: 4,
+        grupo: "G",
+        fin: "03/10/2020",
+        estado: "Cerrado"
+      },
+      {
+        key: 5,
+        grupo: "E",
+        fin: "09/07/2020",
+        estado: "Cerrado"
+      },
+      {
+        key: 6,
+        grupo: "E",
+        fin: "22/07/2019",
+        estado: "Cerrado"
+      }
+    ];
     return (
       <>
-      <Portada
-        materia="Programación de Dispositivos Móviles"
-        carrera="Ing. Sistemas Computacionales"
-        maestro="José Tadeo Rodriguez Solano"
-        grupo="F"
-        periodo="Enero - Junio 2020"
-        // hasTabs
-      >
-        <Typography className={classes.Titulo} variant="h6" component="h6" gutterBottom>
-          Introducción a la programación de dispositivos móviles
-        </Typography>
-        <Accordion defaultExpanded>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Box component="div" className={classes.examenList}>
-              <Box component="div">
-                <Typography>
-                  {/* // Cambiarse a futuro por prop */}
-                  Examen U1 PDM
-                </Typography>
+        <Portada
+          id={this.props.match.params.id}
+          // hasTabs
+        >
+          <Typography className={classes.Titulo} variant="h6" component="h6" gutterBottom>
+            Introducción a la programación de dispositivos móviles
+          </Typography>
+          <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Box component="div" className={classes.examenList}>
+                <Box component="div">
+                  <Typography>
+                    {/* // Cambiarse a futuro por prop */}
+                    Examen U1 PDM
+                  </Typography>
+                </Box>
+                <Box component="div">
+                  <IconButton color="secondary">
+                    <CreateIcon />
+                  </IconButton>
+                  <IconButton color="secondary">
+                    <GroupAddIcon />
+                  </IconButton>
+                  <IconButton color="secondary">
+                    <DeleteIcon />
+                  </IconButton>
+                </Box>
               </Box>
-              <Box component="div">
-                <IconButton color="secondary">
-                  <CreateIcon />
-                </IconButton>
-                <IconButton color="secondary">
-                  <GroupAddIcon />
-                </IconButton>
-                <IconButton color="secondary">
-                  <DeleteIcon />
-                </IconButton>
-              </Box>
-            </Box>
-          </AccordionSummary>
-          <Divider/>
-          <AccordionDetails>
-            <ExamenesTable data={examenes} headers={encabezados} type="examenes"/>
-          </AccordionDetails>
-        </Accordion>
-      </Portada>
-      <FloatingButton reactivos={false}/>
+            </AccordionSummary>
+            <Divider />
+            <AccordionDetails>
+              <ExamenesTable data={examenes} headers={encabezados} type="examenes" />
+            </AccordionDetails>
+          </Accordion>
+        </Portada>
+        <FloatingButton reactivos={false} />
       </>
     );
   }
--- src/containers/Grupos/Grupos.js
+++ src/containers/Grupos/Grupos.js
@@ -15,98 +15,88 @@ import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
 import { Typography } from "@material-ui/core";
 
 class Grupos extends Component {
-    componentDidMount() {
-        this.props.onFetchGrupos(this.props.token, this.props.user.id);
-    }
+  componentDidMount() {
+    this.props.onFetchGrupos(this.props.token, this.props.user.id);
+  }
 
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        const esAdmin = this.props.user.userType == 1;
+    const esAdmin = this.props.user.userType == 1;
 
-        let grupos = <CircularProgress className={classes.spinner} />;
+    let grupos = <CircularProgress className={classes.spinner} />;
 
-        if (!this.props.loading) {
-            grupos =
-                this.props.grupos.length > 0 ? (
-                    this.props.grupos.map((grupo) => {
-                        return (
-                            <Grupo
-                                key={grupo.id}
-                                id={grupo.id}
-                                materia={grupo.nombre}
-                                carrera={grupo.carrera}
-                                maestro={`${this.props.user.nombres} ${this.props.user.apellidoPaterno} ${this.props.user.apellidoMaterno}`}
-                                portada={grupo.fotoPortada}
-                                fotoPerfil={this.props.user.fotoPerfil}
-                            />
-                        );
-                    })
-                ) : (
-                    <Typography>Sin grupos asignados</Typography>
-                );
-        }
-
-        let error = (
-            <Snackbar
-                anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                }}
-                open={this.props.hasError}
-                onClose={() => this.props.onGruposDismissError()}
-                autoHideDuration={4000}
-            >
-                <Alert variant="filled" severity="error">
-                    {this.props.error
-                        ? this.props.error
-                        : "Ha ocurrido un error, favor de intentarlo más tarde"}
-                </Alert>
-            </Snackbar>
-        );
-
-        return (
-            <Grid container spacing={4}>
-                {error}
-                {grupos}
-                {esAdmin ? (
-                    <Link
-                        to={{
-                            pathname: "/admin/grupos",
-                        }}
-                    >
-                        <SpeedDial
-                            ariaLabel="SpeedDial example"
-                            className={classes.speedDial}
-                            icon={<SpeedDialIcon />}
-                        ></SpeedDial>
-                    </Link>
-                ) : null}
-            </Grid>
+    if (!this.props.loading) {
+      grupos =
+        this.props.grupos.length > 0 ? (
+          this.props.grupos.map((grupo) => {
+            return (
+              <Grupo
+                key={grupo.id}
+                id={grupo.id}
+                materia={grupo.nombre}
+                carrera={grupo.carrera}
+                maestro={`${this.props.user.nombres} ${this.props.user.apellidoPaterno} ${this.props.user.apellidoMaterno}`}
+                portada={grupo.fotoPortada}
+                fotoPerfil={this.props.user.fotoPerfil}
+              />
+            );
+          })
+        ) : (
+          <Typography>Sin grupos asignados</Typography>
         );
     }
+
+    let error = (
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        open={this.props.hasError}
+        onClose={() => this.props.onGruposDismissError()}
+        autoHideDuration={4000}
+      >
+        <Alert variant="filled" severity="error">
+          {this.props.error ? this.props.error : "Ha ocurrido un error, favor de intentarlo más tarde"}
+        </Alert>
+      </Snackbar>
+    );
+
+    return (
+      <Grid container spacing={4}>
+        {error}
+        {grupos}
+        {esAdmin ? (
+          <Link
+            to={{
+              pathname: "/admin/grupos"
+            }}
+          >
+            <SpeedDial ariaLabel="SpeedDial example" className={classes.speedDial} icon={<SpeedDialIcon />}></SpeedDial>
+          </Link>
+        ) : null}
+      </Grid>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        grupos: state.grupos.grupos,
-        error: state.grupos.error,
-        hasError: state.grupos.hasError,
-        loading: state.grupos.loading,
-        token: state.auth.token,
-        user: state.auth.user,
-    };
+  return {
+    grupos: state.grupos.grupos,
+    error: state.grupos.error,
+    hasError: state.grupos.hasError,
+    loading: state.grupos.loading,
+    token: state.auth.token,
+    user: state.auth.user
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        onFetchGrupos: (token, userId) =>
-            dispatch(actions.fetchGrupos(token, userId)),
-        onGruposDismissError: () => dispatch(actions.gruposDismissError()),
-    };
+  return {
+    onFetchGrupos: (token, userId) => dispatch(actions.fetchGrupos(token, userId)),
+    onGruposDismissError: () => dispatch(actions.gruposDismissError())
+  };
 };
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(withStyles(useStyles)(Grupos));
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(Grupos));
