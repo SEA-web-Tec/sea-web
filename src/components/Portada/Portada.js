90ea16af03e54ea7152797b9f20c5952808cf116
NightlyFlux <nightlyflux@gmail.com>
2020-12-14 16:41:19 -0800

    Fix fotoPortada

--- src/components/Portada/Portada.js
+++ src/components/Portada/Portada.js
@@ -15,7 +15,7 @@ class Portada extends Component {
             className={classes.cardHeaderBG}
             image={
               this.props.portada
-                ? props.portada
+                ? this.props.portada
                 : "https://media.istockphoto.com/photos/bright-blue-defocused-blurred-motion-abstract-background-picture-id1047234038?k=6&m=1047234038&s=612x612&w=0&h=O1lP8GIn46sboZL5bnMsznd4A1tRNJ7iXm1MMVh5I5c="
             }
             title={this.props.materia}
