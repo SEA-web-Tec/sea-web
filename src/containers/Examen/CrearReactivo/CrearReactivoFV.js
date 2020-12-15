bb313a109145ff480e817b2b5f313179b5492b91
NightlyFlux <nightlyflux@gmail.com>
2020-12-14 22:59:49 -0800

    Fix de pantallas de crear examen, creacion de reactivos (abierto, fv, multiple)

--- src/containers/Examen/CrearReactivo/CrearReactivoFV.js
+++ src/containers/Examen/CrearReactivo/CrearReactivoFV.js
@@ -1,87 +1,137 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "store/actions/index";
 import { updateObject } from "shared/utility";
 import { withStyles } from "@material-ui/core/styles";
 import { useStyles } from "./Styles";
-import { Typography, Grid, FormControl, FormLabel, FormControlLabel, Radio, RadioGroup, InputLabel, Select, MenuItem, TextField, Box, Button } from "@material-ui/core";
+import {
+  Typography,
+  Grid,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+  Box,
+  Button
+} from "@material-ui/core";
 import Portada from "../../../components/Portada/Portada";
+import { http } from "shared/http";
 
-class CrearReactivoAbierto extends Component {
-    state = {
-        unidad: 1,
-        tipo: "falso_verdadero",
-        texto_reactivo: "",
-        respuesta_correcta: true
-    };
+class CrearReactivoFV extends Component {
+  state = {
+    unidad: 1,
+    tipo: "falso_verdadero",
+    texto_reactivo: "",
+    respuesta_correcta: "f"
+  };
 
-    inputChangedHandler = (prop) => (event) => {
-        const updatedObject = updateObject(this.state, {
-        [prop]: event.target.value
-        });
-        this.setState(updatedObject);
-    };
+  inputChangedHandler = (prop) => (event) => {
+    const updatedObject = updateObject(this.state, {
+      [prop]: event.target.value
+    });
+    this.setState(updatedObject);
+  };
 
-    render(props) {
-        const { classes } = this.props;
-        let materias = ['Introducción a interfaces web','Diseño de interfaces gráficas','Creación e implementación de componenetes'];
-        return (
-            <Portada
-            materia="Programación de Dispositivos Móviles"
-            carrera="Ing. Sistemas Computacionales"
-            maestro="José Tadeo Rodriguez Solano"
-            grupo="F"
-            periodo="Enero - Junio 2020">
-            <Typography className={classes.titulo} variant="h6" component="h6">
-                Reactivo Falso / Verdadero
-            </Typography>
-            <form style={{ width: "100%" }}>
-                    <Grid container>
-                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                            <FormControl required variant="outlined" className={classes.formControl}>
-                                <InputLabel id="demo-simple-select-outlined-label">Categoría</InputLabel>
-                                <Select labelId="demo-simple-select-outlined-label" id="demo-simple-select-outlined" label="Categoría">
-                                {materias.map((m) => {
-                                        return (<MenuItem key={m} value={m}>{m}</MenuItem>);
-                                    })}
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                            <FormControl required variant="outlined" className={classes.formControl}>
-                                <TextField 
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    multiline
-                                    fullWidth
-                                    rows={4}
-                                    id="texto_reactivo"
-                                    name="texto_reactivo"
-                                    label="Reactivo"
-                                    value={this.state.texto_reactivo}
-                                    onChange={this.inputChangedHandler("texto_reactivo")}/>
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                            <FormControl required variant="outlined" className={classes.formControl}>
-                                <FormLabel component="legend">Respuesta</FormLabel>
-                                <RadioGroup row aria-label="position" name="position" defaultValue="top">
-                                    <FormControlLabel value="f" control={<Radio color="primary"/>}
-                                    label="Falso" labelPlacement="end"/>
-                                    <FormControlLabel value="v" control={<Radio color="primary"/>}
-                                    label="Verdadero" labelPlacement="end"/>
-                                </RadioGroup>
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                    <Box className={classes.center}>
-                        <Button variant="contained" color="primary">
-                            Crear reactivo
-                        </Button>
-                    </Box>
-            </form>
-            </Portada>
-        );
+  submitHandler = (event) => {
+    event.preventDefault();
+
+    http
+      .post(
+        "/reactivos/crear",
+        {
+          id_materia: this.props.match.params.id,
+          unidad: this.state.unidad,
+          tipo: this.state.tipo,
+          texto_reactivo: this.state.texto_reactivo,
+          respuesta_correcta: this.state.respuesta_correcta
+        },
+        {
+          headers: { Authorization: `Bearer ${this.props.token}` }
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  render(props) {
+    const { classes } = this.props;
+    let unidades = [];
+    for (let i = 1; i <= this.props.grupos.find((grupo) => grupo.id == this.props.match.params.id).unidades; i++) {
+      unidades.push(i);
     }
+
+    return (
+      <Portada id={this.props.match.params.id}>
+        <Typography className={classes.titulo} variant="h6" component="h6">
+          Reactivo Falso/Verdadero
+        </Typography>
+        <form className={classes.form} onSubmit={this.submitHandler}>
+          <FormControl required fullWidth margin="normal" variant="outlined">
+            <InputLabel id="unidadLabel">Unidad</InputLabel>
+            <Select
+              id="unidad"
+              name="unidad"
+              value={this.state.unidad}
+              onChange={this.inputChangedHandler("unidad")}
+              labelId="unidadLabel"
+              label="Unidad"
+            >
+              {unidades.map((unidad, i) => (
+                <MenuItem key={i} value={unidad}>
+                  {unidad}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="texto_reactivo"
+            name="texto_reactivo"
+            label="Pregunta"
+            value={this.state.texto_reactivo}
+            onChange={this.inputChangedHandler("texto_reactivo")}
+          />
+          <FormControl required variant="outlined" className={classes.formControl}>
+            <FormLabel component="legend">Respuesta</FormLabel>
+            <RadioGroup
+              row
+              aria-label="position"
+              name="position"
+              value={this.state.respuesta_correcta}
+              onChange={this.inputChangedHandler("respuesta_correcta")}
+            >
+              <FormControlLabel value="f" control={<Radio color="primary" />} label="Falso" labelPlacement="end" />
+              <FormControlLabel value="v" control={<Radio color="primary" />} label="Verdadero" labelPlacement="end" />
+            </RadioGroup>
+          </FormControl>
+          <Box className={classes.center}>
+            <Button variant="contained" color="primary" type="submit">
+              Crear reactivo
+            </Button>
+          </Box>
+        </form>
+      </Portada>
+    );
+  }
 }
 
-export default withStyles(useStyles)(CrearReactivoAbierto);
+const mapStateToProps = (state) => {
+  return { token: state.auth.token, user: state.auth.user, grupos: state.grupos.grupos };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchGrupos: (token, userId) => dispatch(actions.fetchGrupos(token, userId))
+  };
+};
+
+export default connect(mapStateToProps)(withStyles(useStyles)(CrearReactivoFV));
